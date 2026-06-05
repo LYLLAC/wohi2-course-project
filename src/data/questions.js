b3a1ea681bb93151cf97be14365a50d1b0975d0c
LYLLAC <angelaziegler477@gmail.com>
2026-06-05 15:47:12 +0800

    Creating questions API

---
+++ src/data/questions.js
@@ -0,0 +1,28 @@
+const questions = [
+    {
+        id: 1,
+        question: "What is the name for a portable computer that folds?",
+        answer: "Laptop",
+        keywords: ["device"]
+    },
+    {
+        id: 2,
+        question: "What does CPU stand for?",
+        answer: "Central Processing Unit",
+        keywords: ["unit"]
+    },
+    {
+        id: 3,
+        question: "What does URL stand for?",
+        answer: "Uniform Resource Locator",
+        keywords: ["locator"]
+    },
+    {
+        id: 4,
+        question: "What does HTTP stand for?",
+        answer: "Hypertext Transfer Protocol",
+        keywords: ["http"]
+    }
+];
+
+module.exports = questions;
